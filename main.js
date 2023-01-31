a46cd40d9ed62e51a14d505d6c31e9fd3ab34887
gholamreza-hasnzade <gholamreza15402@gmail.com>
2023-01-31 16:27:49 +0330

    example factory

---
+++ main.js
@@ -0,0 +1,34 @@
+/* const FactoryFunction = (string) => {
+    const capitalizeString = () => string.toUpperCase();
+    const printString = () => console.log(`----${capitalizeString()}----`);
+    return { printString };
+};
+
+const taco = FactoryFunction("taco");
+
+printString(); // ERROR!!
+capitalizeString(); // ERROR!!
+taco.capitalizeString(); // ERROR!!
+taco.printString(); // this prints "----TACO----"
+ */
+/* const counterCreator = () => {
+    let count = 0;
+    return () => {
+        console.log(count);
+        count++;
+    };
+};
+
+const counter = counterCreator();
+
+counter(); // 0
+counter(); // 1
+counter(); // 2
+counter(); // 3 */
+var width;
+function setWidth() {
+  width = 100;
+  console.log(width);
+}
+setWidth();
+console.log(width);
